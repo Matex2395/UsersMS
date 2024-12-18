b57bb2074a4e74b29858dc4bdbfd8b048a940d9b
Matex2395 <mateorgafjo@gmail.com>
2024-12-18 09:58:08 -0500

    Instalación de Paquetes NuGet Definición de Modelos - User & Role

---
+++ Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginMS.Models
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int vli_id { get; set; }
+        [Required]
+        public required string vls_name { get; set; }
+        public string? vls_description { get; set; }
+    }
+}
