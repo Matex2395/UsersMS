1f36482080cd9cb44e18a062d44a97fb1731d353
Matex2395 <mateorgafjo@gmail.com>
2024-12-23 10:42:18 -0500

    Implementación DbContext en Program.cs & Configurar JWT en appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using LoginMS.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MSDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 
 var app = builder.Build();
 
