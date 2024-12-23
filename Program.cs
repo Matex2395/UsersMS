2b934a4d1863b795cfb84fed365f455ec52f7e1c
Matex2395 <mateorgafjo@gmail.com>
2024-12-23 11:46:23 -0500

    Configurar parámetros de autenticación de JWT en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using LoginMS.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using LoginMS.Custom;
+using LoginMS.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +18,37 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MSDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 
+// Utils Instantiation
+builder.Services.AddSingleton<Utils>();
+
+// JWT Authentication Configuration
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+
+        /* ValidateIssuer allows or prohibits external apps to access this API URL when published */
+        ValidateIssuer = false, // No access from external apps
+
+        /* We can specify which systems (Apps, Server) can access this API URL */
+        ValidateAudience = false, // No access from external apps (It can be configured)
+
+        /* Use time limit validation for Token Lifetime and Clock-related specifications */
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]!))
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +59,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
