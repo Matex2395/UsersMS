221d0c27b7e9ae280a022dfe48cfd4d3cdebd084
MateoRosero <mateorgafjo@gmail.com>
2024-12-26 13:53:28 -0500

    Implementar Servicio de envío de emails

---
+++ Services/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+
+namespace LoginMS.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "mateo.rosero@fisagrp.com";
+            var pw = "W8lS#JKMjN7ifd&1";
+
+            var client = new SmtpClient("smtp-mail.outlook.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: mail,
+                                to: email,
+                                subject,
+                                message
+                                ));
+        }
+    }
+}
---
+++ Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LoginMS.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
