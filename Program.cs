f36ff5b5e059292d99d4ab225808d75bc6c993c5
MateoRosero <mateorgafjo@gmail.com>
2024-12-26 12:31:23 -0500

    Revert "Crear clase estática para FluentEmail & Configurar parámetros en ella"
    This reverts commit 2b8f4a5689c55176550bf779219f0e13e881e107.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddFluentEmail(builder.Configuration);
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
