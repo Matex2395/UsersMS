6ff8dab98052e850f24fa1cc762fa2ae9eaec1d3
Matex2395 <mateorgafjo@gmail.com>
2024-12-30 13:52:33 -0500

    Importar DbContext de Base de Datos oficial

---
+++ Models/DbAb0bdeTalentseedsContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginMS.Models;
+
+public partial class DbAb0bdeTalentseedsContext : DbContext
+{
+    public DbAb0bdeTalentseedsContext()
+    {
+    }
+
+    public DbAb0bdeTalentseedsContext(DbContextOptions<DbAb0bdeTalentseedsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TfaRol> TfaRols { get; set; }
+
+    public virtual DbSet<TfaUser> TfaUsers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConStr");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("Modern_Spanish_CI_AI");
+
+        modelBuilder.Entity<TfaRol>(entity =>
+        {
+            entity.HasKey(e => e.RolId).HasName("PK__TFA_ROL__5402365444BEE72A");
+
+            entity.ToTable("TFA_ROL");
+
+            entity.Property(e => e.RolId).HasColumnName("rolID");
+            entity.Property(e => e.RolDescription)
+                .HasMaxLength(500)
+                .IsUnicode(false)
+                .HasColumnName("rolDescription");
+            entity.Property(e => e.RolName)
+                .HasMaxLength(50)
+                .HasColumnName("rolName");
+        });
+
+        modelBuilder.Entity<TfaUser>(entity =>
+        {
+            entity.HasKey(e => e.UsersId).HasName("PK__TFA_USER__788FDD2552307940");
+
+            entity.ToTable("TFA_USERS");
+
+            entity.Property(e => e.UsersId).HasColumnName("usersID");
+            entity.Property(e => e.Contrasenia)
+                .HasMaxLength(999)
+                .IsUnicode(false)
+                .HasColumnName("contrasenia");
+            entity.Property(e => e.RolId).HasColumnName("rolID");
+            entity.Property(e => e.RolIdaddional).HasColumnName("rolIDAddional");
+            entity.Property(e => e.UserEmail)
+                .HasMaxLength(100)
+                .HasColumnName("userEmail");
+            entity.Property(e => e.UserLastName)
+                .HasMaxLength(50)
+                .HasColumnName("userLastName");
+            entity.Property(e => e.UserName)
+                .HasMaxLength(50)
+                .HasColumnName("userName");
+            entity.Property(e => e.UserPoints).HasColumnName("userPoints");
+
+            entity.HasOne(d => d.Rol).WithMany(p => p.TfaUserRols)
+                .HasForeignKey(d => d.RolId)
+                .HasConstraintName("FK_userRol");
+
+            entity.HasOne(d => d.RolIdaddionalNavigation).WithMany(p => p.TfaUserRolIdaddionalNavigations)
+                .HasForeignKey(d => d.RolIdaddional)
+                .HasConstraintName("FK_userRolAddional");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/TfaUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoginMS.Models;
+
+public partial class TfaUser
+{
+    public int UsersId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserLastName { get; set; } = null!;
+
+    public string UserEmail { get; set; } = null!;
+
+    public int UserPoints { get; set; }
+
+    public int? RolId { get; set; }
+
+    public int? RolIdaddional { get; set; }
+
+    public string Contrasenia { get; set; } = null!;
+
+    public virtual TfaRol? Rol { get; set; }
+
+    public virtual TfaRol? RolIdaddionalNavigation { get; set; }
+}
